8ea6e71fad6d54f462e488f4d3b8579e4bf76dc9
Carlos Santana <czantany@MacBook-Pro-de-Carlos.local>
2016-04-14 00:11:38 -0700

    Adding movies with filter

--- src/public/js/movies/movies.controller.js
+++ src/public/js/movies/movies.controller.js
@@ -3,6 +3,7 @@
 
     define(function(require) {
         var angular = require('angular');
+        var _ = require('lodash');
 
         angular
             .module('moviesApp')
@@ -15,11 +16,27 @@
         function MoviesAppController(moviesDataService) {
             var vm = this;
 
+            vm.filterByCategory = filterByCategory;
+
             moviesDataService
                 .getMovies()
                 .then(function(response) {
                     vm.movies = response.movies;
+                    vm.filteredMovies = vm.movies;
+                    vm.categories = _.uniq(vm.movies.map(function(movie) {
+                        return movie.category;
+                    }));
                 });
+
+            function filterByCategory(category) {
+                vm.filteredMovies = vm.movies;
+
+                if (category) {
+                    vm.filteredMovies = vm.movies.filter(function(item) {
+                        return item.category === category;
+                    });
+                }
+            }
         }
     });
 })();
